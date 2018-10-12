236995b1de99b4c30ee2b4eb7a08e65f1be745a4
daanimy12 <daanimy123@gmail.com>
2018-10-12 15:17:08 -0500

    HOLA

--- Cafeteria/src/app/recipes/recipes.model.ts
+++ Cafeteria/src/app/recipes/recipes.model.ts
@@ -1,10 +1,10 @@
 import { Ingredient } from "../shared/ingrediente.model";
 
 export class Recipe{
-  public name: string;
-  public description: string;
-  public imagePath: string;
-  public ingredients: Ingredient[];
+  private name: string;
+  private description: string;
+  private imagePath: string;
+  private ingredients: Ingredient[];
   constructor(name: string, description: string, imagePath: string,ingredients:Ingredient[]){
       this.name = name;
       this.description = description;
